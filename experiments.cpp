1947ff348bad50359ab24c0048174ad46df7929f
Thomas Mailund <mailund@birc.au.dk>
2022-12-15 12:48:55 +0100

    more experiments

--- experiments.cpp
+++ experiments.cpp
@@ -3,14 +3,14 @@
 
 // Iterating in reverse...
 template <typename iterable> struct reverse_range {
-  iterable &wrapped;
+  iterable wrapped;
   auto begin() { return std::rbegin(wrapped); }
   auto end() { return std::rend(wrapped); }
 };
 
 template <typename iterable>
-struct reverse_range<iterable> reverse(iterable &wrapped) {
-  return reverse_range<iterable>{wrapped};
+struct reverse_range<iterable> reverse(iterable &&wrapped) {
+  return reverse_range<iterable>{wrapped}; // FIXME: check if I copy here!!!
 }
 
 // Enumerate
@@ -33,17 +33,12 @@ struct enumerate_iterator {
   }
 };
 template <typename iterable> struct enumerate_range {
-  iterable &wrapped;
-  using iter_type = typename iterable::iterator;
-  enumerate_iterator<iter_type> begin() {
-    return enumerate_iterator<iter_type>(std::begin(wrapped));
-  }
-  enumerate_iterator<iter_type> end() {
-    return enumerate_iterator<iter_type>(std::end(wrapped));
-  }
+  iterable wrapped;
+  auto begin() { return enumerate_iterator(std::begin(wrapped)); }
+  auto end() { return enumerate_iterator(std::end(wrapped)); }
 };
 template <typename iterable>
-struct enumerate_range<iterable> enumerate(iterable &wrapped) {
+struct enumerate_range<iterable> enumerate(iterable &&wrapped) {
   return enumerate_range<iterable>{wrapped};
 }
 
@@ -66,17 +61,20 @@ int main() {
   }
   std::cout << std::endl;
 
-  /* This doesn't work because enumerate wants an lvalue. That is
-     because it wants to hold a reference (which it should, since we)
-     don't want to copy the vector with enumerate(v), but here the
-     reference is in reverse(v) and it *should* have a copy for reverse(v).
-     I don't know how to specialise this yet...
+  for (auto [i, j] : enumerate(reverse(v))) {
+    std::cout << '(' << i << ',' << j << ") ";
+  }
+  std::cout << std::endl;
 
-    for (auto [i, j] : enumerate(reverse(v))) {
-        std::cout << '(' << i << ',' << j << ") ";
-    }
-    std::cout << std::endl;
-*/
+/* This doesn't work yet, because enumerate doesn't know how to
+ iterate in reverse. That would require that I figure out the
+ length of the iteration to begin with... */
+#if 0
+  for (auto [i, j] : reverse(enumerate(v))) {
+    std::cout << '(' << i << ',' << j << ") ";
+  }
+  std::cout << std::endl;
+#endif
 
   return 0;
 }
